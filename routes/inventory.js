4a835bdfa9cd51557c6ec09eb5ce27e017f6b5a8
Laniban <laniban321@gmail.com>
2026-07-16 14:22:52 +0530

    fix: require path module for template download

--- routes/inventory.js
+++ routes/inventory.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const router = express.Router();
 const db = require('../config/db');
 const { authenticateToken, requireAdmin } = require('../middlewares/auth');
